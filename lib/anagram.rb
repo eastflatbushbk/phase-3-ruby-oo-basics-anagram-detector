2c11767503e20475c17fcc6ef139383c99af0f71
Robert Sainton <robertsainton@outlook.com>
2022-11-14 17:41:22 -0500

    completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+        end
+        def match(array)
+           array.filter{|item| item.split("").sort == @word.split("").sort }
+        end
+end
